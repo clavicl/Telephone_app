f9cde60be57f09972cfb7882fe451ce4fc8c5e08
jandrewyoung <jay10@my.fsu.edu>
2013-10-23 21:13:16 -0400

    Another Update
    UI is pretty much implemented as far as navigation goes. You guys can
    work on the programming now, and I'll make it look nice over the
    weekend.

---
+++ StoryApp/src/edu/fsu/mobile/storyapp/CreateStoryActivity.java
@@ -0,0 +1,14 @@
+package edu.fsu.mobile.storyapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CreateStoryActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.create_view);
+	}
+
+}
--- StoryApp/src/edu/fsu/mobile/storyapp/MainActivity.java
+++ StoryApp/src/edu/fsu/mobile/storyapp/MainActivity.java
@@ -2,7 +2,9 @@ package edu.fsu.mobile.storyapp;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
 
 
 // this is a test - Ryan Alain
@@ -13,6 +15,26 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 	}
+
+public void createStory(View view){
+	Intent intent = new Intent(this, CreateStoryActivity.class);
+	startActivity(intent);
+}
+
+public void checkCreated(View view){
+	Intent intent = new Intent(this, CheckCreatedActivity.class);
+	startActivity(intent);
+}
+
+public void checkContributed(View view){
+	Intent intent = new Intent(this, CheckContributedActivity.class);
+	startActivity(intent);
+}
+
+public void checkRequest(View view){
+	Intent intent = new Intent(this, CheckRequestActivity.class);
+	startActivity(intent);
+}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
