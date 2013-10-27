42563634724a06883dceedafb9afbe87782a6763
David E. <daesgu90@gmail.com>
2013-10-26 20:27:55 -0400

    update
    some stuff I've been working on

--- StoryApp/src/edu/fsu/mobile/storyapp/CreateSettingsDialog.java
+++ StoryApp/src/edu/fsu/mobile/storyapp/CreateSettingsDialog.java
@@ -1,7 +1,61 @@
 package edu.fsu.mobile.storyapp;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class CreateSettingsDialog  extends DialogFragment{
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
 
+        try {
+            dListener = (CreateSettingsDialogListener) activity;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(activity.toString()
+                    +" must implement CreateSettingsDialogListener");
+        }
+    }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+
+        View view = LayoutInflater.from(getActivity()).inflate(R.layout.create_settings_dialog,null);
+
+        final EditText max_words_EditText = (EditText)view.findViewById(R.id.max_words);
+        final EditText next_contributor_EditText = (EditText)view.findViewById(R.id.next_contributor);
+
+        //builder.setView(inflater.inflate(R.layout.dialog_layout,null));
+        builder.setView(view);
+        builder.setPositiveButton("Create", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int i) {
+                String max_words = max_words_EditText.getText().toString();
+                String next_contributor = next_contributor_EditText.getText().toString();
+                dListener.onDialogClick(max_words,next_contributor);
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int i) {
+            }
+        });
+        return builder.create();
+    }
+
+    public interface CreateSettingsDialogListener {
+        public void onDialogClick(String maxWords, String nextContributor);
+    }
+
+    CreateSettingsDialogListener dListener;
 }
--- StoryApp/src/edu/fsu/mobile/storyapp/CreateStoryActivity.java
+++ StoryApp/src/edu/fsu/mobile/storyapp/CreateStoryActivity.java
@@ -1,14 +1,142 @@
 package edu.fsu.mobile.storyapp;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.telephony.TelephonyManager;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class CreateStoryActivity extends Activity {
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.StatusLine;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPut;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CreateStoryActivity extends Activity
+    implements CreateSettingsDialog.CreateSettingsDialogListener{
+    private String Title;
+    private String Story;
+    private String Pnumber;
+    private String Max_words;
+    private String Next_contributor;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.create_view);
+
+        //get phone number
+        Pnumber = getMyPhoneNumber();
+
+        Button createStory_Button = (Button)findViewById(R.id.createButton);
+        createStory_Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CreateSettingsDialog dialog = new CreateSettingsDialog();
+
+                Title = ((EditText)findViewById(R.id.newStoryTitle)).getText().toString().trim();
+                Story = ((EditText)findViewById(R.id.newStoryText)).getText().toString().trim();
+
+                dialog.show(getFragmentManager(),"settingsDialog");
+            }
+        });
 	}
+
+    private String getMyPhoneNumber(){
+        TelephonyManager mTelephonyMgr;
+        mTelephonyMgr = (TelephonyManager)
+                getSystemService(Context.TELEPHONY_SERVICE);
+        if (mTelephonyMgr.getLine1Number() != null)
+            return mTelephonyMgr.getLine1Number().substring(1);
+        else
+            return "0000000000";
+    }
+
+    public void onDialogClick(String maxWords, String nextContributor)
+    {
+        Max_words = maxWords;
+        Next_contributor = nextContributor;
+        createStory();
+        finish();
+    }
+
+    private void createStory()
+    {
+        StringBuilder builder = new StringBuilder();
+        HttpClient client = new DefaultHttpClient();
+        String amp = "&";
+        String base = "http://myligaapi.elementfx.com/teleApp/editStory.php";//?flag=add&" +
+                      // "story=";
+        String url = base +"?flag=add" + amp +
+                "story=" + Story + amp +
+                "words_max="+ Max_words + amp +
+                "next=" + Next_contributor + amp +
+                "creator=" + Pnumber + amp +
+                "title=" + Title;
+        url = url.replaceAll(" ", "%20");
+        HttpGet httpGet = new HttpGet(url);
+
+        HttpPut httpPut = new HttpPut(base);
+        List<NameValuePair> pairs = new ArrayList<NameValuePair>();
+        pairs.add(new BasicNameValuePair("flag","add"));
+        pairs.add(new BasicNameValuePair("story",Story));
+        pairs.add(new BasicNameValuePair("words_max",Max_words));
+
+        httpPut.setEntity(new UrlEncodedFormEntity(pairs));
+
+
+        try {
+            HttpResponse response = client.execute(httpGet);
+            StatusLine statusLine = response.getStatusLine();
+            int statusCode = statusLine.getStatusCode();
+            if (statusCode == 200) {
+                HttpEntity entity = response.getEntity();
+                InputStream content = entity.getContent();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(content));
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    builder.append(line);
+                }
+            } else {
+                Log.e(CreateStoryActivity.class.toString(), "Failed to download file");
+            }
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try{
+            JSONObject jsonObject = new JSONObject(builder.toString());
+            String flag = jsonObject.getString("flag");
+            if (flag.equals("1"))
+            {
+                Toast.makeText(getApplicationContext(),"Story created successful",Toast.LENGTH_LONG);
+            }
+            else
+                Toast.makeText(getApplicationContext(),"Failed to create story, please try again",Toast.LENGTH_LONG);
+        }catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
 
 }
